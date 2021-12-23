7bb7188b3c25c058de7aaf1b49e26f63d790e391
Jerry Fitzner <jfitzner1@gmail.com>
2021-12-23 14:53:46 -0600

    trial 1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob'; 
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+};
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+        return bestCustomer;
+};
+
+function overwriteBestCustomer(){
+    var bestCustomer = 'maybe bob'
+        return bestCustomer;
+}
+
+
+
+
+
